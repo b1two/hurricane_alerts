0e831d7bd133b413aa5b5fb8283046ec9aa78706
Pierre Milioni <b02c.out@gmail.com>
2020-02-07 11:52:46 +0100

    Code cleaning, adding a longer hurricane report

--- hurricane.py
+++ hurricane.py
@@ -26,42 +26,78 @@ logger = logging.getLogger("bouygues_pysms")
 logger.setLevel(logging.WARNING)
 
 
+class hurricane_api:
+
+    def __init__(self):
+        self.session = requests.Session()
+
+        #self.url_hurricane = "https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=5"
+        self.url_hurricane = "https://web.archive.org/web/20190903123638/https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=5"
+        self.url_jenny = "https://www.volcanodiscovery.com/kick-em-jenny/news/70958/Kick-em-Jenny-volcano-West-Indies-Grenada-activity-update-Elevated-seismicity.html"
+
+        self.data_hurricane = ""
+        self.data_jenny = ""
+
+        self.data_clean_hurricane = ""
+
+    def __clean_data_hurricane(self):
+        replacements = {
+            "$$": "",
+            "\n": " ",
+        }
+        temp = self.data_hurricane
+        for pattern, new_val in replacements.items():
+            temp = temp.replace(pattern, new_val)
+
+        temp = re.sub(r"\s+", " ", temp).strip()
+        self.data_clean_hurricane = temp
+
+    def update_forecast(self):
+        """ Retrieving data from huricane center """
+        hurricane_raw = self.session.get(self.url_hurricane)
+        # We cannot use [.\s] as . is considered litteral in []
+        self.data_hurricane = re.search(r"For the North Atlantic\.\.\.Caribbean Sea and the Gulf of Mexico:\n\n([ -~\s]*)\s*Forecaster",
+                                        hurricane_raw.content.decode('utf-8')).group(1)
+
+        """ Retrieving data for Kick'em Jenny """
+        jenny_raw = self.session.get(self.url_jenny)
+        self.data_jenny = re.search(r"\(([0-9]) out of 5\)",
+                                    jenny_raw.content.decode('utf-8')).group(1)
+
+    def is_there_hurricane(self):
+        return "Tropical cyclone formation is not expected during the next 5 days." not in self.data_hurricane
+
+    def get_forecast_short(self):
+        if self.is_there_hurricane():
+            return "Attention, il peut y avoir un cyclone de prévu dans les 5 prochains jours. Echelle " + self.data_jenny + "/5 Kick'em Jenny."
+        else:
+            return "Pas de cyclone prévu dans les 5 prochains jours. Echelle " + self.data_jenny + "/5 Kick'em Jenny."
+
+    def get_forecast_full(self):
+        if self.is_there_hurricane():
+            self.__clean_data_hurricane()
+            return self.data_clean_hurricane + ". Echelle " + self.data_jenny + "/5 Kick'em Jenny."
+        else:
+            return "Pas de cyclone prévu dans les 5 prochains jours. Echelle " + self.data_jenny + "/5 Kick'em Jenny."
+
+
+
 def main():
-    """ Retrieving data from huricane center """
-    session_hurricane = requests.Session()
-    response_hurricane = session_hurricane.get("https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=5")
-    matches_hurricane = re.search(r"For the North Atlantic\.\.\.Caribbean Sea and the Gulf of Mexico:\n\n([ -~\s]*)\n\n\$\$",
-                        response_hurricane.content.decode('utf-8'))
-    if matches_hurricane:
-        infos_hurricane = matches_hurricane.groups()[0]
-        hurricane = infos_hurricane != "Tropical cyclone formation is not expected during the next 5 days."
-    else:
-        hurricane = True
+    """ Retrieving data """
+    hurricane_center = hurricane_api()
+    hurricane_center.update_forecast()
 
-    """ Retrieving data for Kick'em Jenny """
-    session_volcano = requests.Session()
-    response_volcano = session_volcano.get(r"https://www.volcanodiscovery.com/kick-em-jenny/news/70958/Kick-em-Jenny-volcano-West-Indies-Grenada-activity-update-Elevated-seismicity.html")
-    matches_volcano = re.search(r"\(([0-9]) out of 5\)",
-                                response_volcano.content.decode('utf-8'))
-    status_volcano = matches_volcano.groups()[0]
-
-    """ Sending info """
     if DEBUG:
-        print("hurricane : ", hurricane)
-        print("Alert level : ", status_volcano)
+        print("Full forecast :")
+        print(hurricane_center.get_forecast_full())
+        print(len(hurricane_center.get_forecast_full()))
+        print("\nShort forecast :")
+        print(hurricane_center.get_forecast_short())
 
-        if not hurricane:
-            print("client.send(\"Pas de cyclone prévu dans les 5 prochains jours. Echelle " + status_volcano + "/5 Kick'em Jenny.\", " + str(RECIPIENTS) + ")")
-        else:
-            print("client.send(\"Attention, il peut y avoir un cyclone de prévu dans les 5 prochains jours. Echelle " + status_volcano + "/5 Kick'em Jenny.\", " + str(RECIPIENTS) + ")")
     else:
+        """ Sending SMSs """
         client = BouyguesClient(MY_NAME, MY_PHONE_NUMBER, MY_PASSWORD)
-
-        if not hurricane:
-            client.send("Pas de cyclone prévu dans les 5 prochains jours. Echelle " + status_volcano + "/5 Kick'em Jenny.", RECIPIENTS)
-        else:
-            client.send("Attention, il peut y avoir un cyclone de prévu dans les 5 prochains jours. Echelle " + status_volcano + "/5 Kick'em Jenny.", RECIPIENTS)
-
+        client.send(hurricane_center.get_forecast_short, RECIPIENTS)
 
 if __name__ == "__main__":
     main()
